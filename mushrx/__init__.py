3b5e6558547d2a00c7cc4ba892025994970032e9
spatterss135 <spatterss135@gmail.com>
2022-05-07 09:34:19 -0500

    ddd

--- mushrx/__init__.py
+++ mushrx/__init__.py
@@ -10,11 +10,8 @@ def create_app():
 
 
     # config
-        uri = os.getenv("DATABASE_URL")  # or other relevant config var
-    if uri and uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
-    # rest of connection code using the connection string `uri`
-    app.config['SQLALCHEMY_DATABASE_URI'] = uri
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
